ced0e57d6dc627b0051a2bd88edbf19820879bdd
Haitao Lu <lht19900714@gmail.com>
2023-12-12 04:24:20 +0800

    Add VSCode settings and Blurry Loading page

---
+++ 05-blurry-loading/script.js
@@ -0,0 +1,23 @@
+const bg = document.querySelector(".bg");
+const loadingText = document.querySelector(".loading-text");
+
+let load = 0;
+
+let interval = setInterval(blurring, 20);
+
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(interval);
+  }
+
+  loadingText.innerText = `${load}%`;
+  loadingText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
